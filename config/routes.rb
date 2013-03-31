9e5e646001c98f26069c2cd3b13b43d8223d3b5b
Ahmedrali <ahmedrali2@gmail.com>
2013-03-31 05:05:57 -0700

    Edit routes for rails4

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Mercury::Engine.routes.draw do
-  match '/editor(/*requested_uri)' => "mercury#edit", :as => :mercury_editor
+  match '/editor(/*requested_uri)' => "mercury#edit", :as => :mercury_editor, :via => :post
 
   scope '/mercury' do
-    match ':type/:resource' => "mercury#resource"
-    match 'snippets/:name/options' => "mercury#snippet_options"
-    match 'snippets/:name/preview' => "mercury#snippet_preview"
+    match ':type/:resource' => "mercury#resource", :via => :get
+    match 'snippets/:name/options' => "mercury#snippet_options", :via => :get
+    match 'snippets/:name/preview' => "mercury#snippet_preview", :via => :get
   end
 end
